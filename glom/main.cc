35d14ebe5af405d70aa873f8127125f45052fec3
Daniel Holbach <dh@mailempfang.de>
2005-07-30 12:49:40 +0000

    Changed the gnome_program_init() call again. Apparently it was crashing on
    2005-07-30  Daniel Holbach <dh@mailempfang.de>
    
            * glom/main.cc: Changed the gnome_program_init() call again.
            Apparently it was crashing on AMD64, though we do not know
            why.

--- glom/main.cc
+++ glom/main.cc
@@ -86,9 +86,11 @@ main(int argc, char* argv[])
   try
   {
     //Initialize gnome_program, so that we can use gnome_help_display().    
-    gnome_program_init (PACKAGE, VERSION, LIBGNOME_MODULE, argc, argv,
-                            GNOME_PROGRAM_STANDARD_PROPERTIES, 0, GNOME_PARAM_NONE);
-
+    gnome_program_init(PACKAGE_NAME, PACKAGE_VERSION, LIBGNOME_MODULE, argc, argv,
+		    GNOME_PARAM_HUMAN_READABLE_NAME, _("Glom"),
+		    GNOME_PROGRAM_STANDARD_PROPERTIES, NULL);
+    
+    
     Gtk::Main mainInstance(argc, argv, context);
     Bakery::init();
     Gnome::Canvas::init();
