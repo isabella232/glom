753c096d62b5f49210927b62d1f66b7f0e64aa64
Murray Cumming <murrayc@murrayc.com>
2006-01-10 19:25:09 +0000

    Convert to the new gnome-doc-utils (xml2po) system, with a dummy de
    2006-01-10  Murray Cumming  <murrayc@murrayc.com>
    
            * .cvsignore:
            * Makefile.am:
            * autogen.sh:
            * configure.in:
            * docs/user-guide/.cvsignore:
            * docs/user-guide/C/Makefile.am:
            * docs/user-guide/C/glom-C.omf:
            * docs/user-guide/C/glom.xml:
            * docs/user-guide/Makefile.am:
            * docs/user-guide/de/de.po:
            * docs/user-guide/glom-C.omf.in:
            * gnome-doc-utils.make: Convert to the new
            gnome-doc-utils (xml2po) system, with a
            dummy de translation, though I get warnings about
            the path to the images in figures.

--- autogen.sh
+++ autogen.sh
@@ -58,6 +58,8 @@ echo "- intltoolize."		&& \
   intltoolize --copy --force	&& \
 echo "- libtoolize."		&& \
   libtoolize --copy --force --automake 	&& \
+echo "- gnome-doc-prepare."	&& \
+  gnome-doc-prepare --force 	&& \
 echo "- aclocal"		&& \
   aclocal -I "$srcdir/macros" $ACLOCAL_FLAGS && \
 echo "- autoheader"		&& \
