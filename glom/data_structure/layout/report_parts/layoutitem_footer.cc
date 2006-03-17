af63cf03b88f537fe45150c81cbb42c5ed6e93aa
Murray Cumming <murrayc@murrayc.com>
2006-03-17 10:28:56 +0000

    Add header and footer parts for reports, though they are not used yet. Add
    2006-03-17  Murray Cumming  <murrayc@murrayc.com>
    
            * glom/data_structure/layout/report_parts/Makefile.am:
            * glom/data_structure/layout/report_parts/layoutitem_footer.cc:
            * glom/data_structure/layout/report_parts/layoutitem_footer.h:
            * glom/data_structure/layout/report_parts/layoutitem_header.cc:
            * glom/data_structure/layout/report_parts/layoutitem_header.h: Add
            header and footer parts for reports, though they are not used yet.
            * po/POTFILES.in: Add the new files.

---
+++ glom/data_structure/layout/report_parts/layoutitem_footer.cc
@@ -0,0 +1,54 @@
+/* Glom
+ *
+ * Copyright (C) 2001-2004 Murray Cumming
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+ 
+#include "layoutitem_footer.h"
+#include <glibmm/i18n.h>
+
+LayoutItem_Footer::LayoutItem_Footer()
+{
+}
+
+LayoutItem_Footer::LayoutItem_Footer(const LayoutItem_Footer& src)
+: LayoutGroup(src)
+{
+}
+
+LayoutItem_Footer::~LayoutItem_Footer()
+{
+}
+
+LayoutItem* LayoutItem_Footer::clone() const
+{
+  return new LayoutItem_Footer(*this);
+}
+
+
+LayoutItem_Footer& LayoutItem_Footer::operator=(const LayoutItem_Footer& src)
+{
+  LayoutGroup::operator=(src);
+
+  return *this;
+}
+
+Glib::ustring LayoutItem_Footer::get_part_type_name() const
+{
+  return _("Footer");
+}
+
