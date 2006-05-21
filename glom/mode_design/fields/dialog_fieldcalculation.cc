aace65cfdd72c92e7687ea728ebd2b7c11fa7c93
Murray Cumming <murrayc@murrayc.com>
2006-05-21 10:52:17 +0000

    Remove the .png extension from the icon name, as advised by
    2006-05-21  Murray Cumming  <murrayc@murrayc.com>
    
            * glom.desktop.in.in: Remove the .png extension from the
            icon name, as advised by http://live.gnome.org/GnomeGoals/AppIcon .
            * glom/mode_design/fields/dialog_fieldcalculation.cc: Remove the [?]
            * icons/Makefile.am: Add EXTRA_DIST stuff to fix distcheck
            from the list of dependent relationships. It looks like an errror.

--- glom/mode_design/fields/dialog_fieldcalculation.cc
+++ glom/mode_design/fields/dialog_fieldcalculation.cc
@@ -112,7 +112,7 @@ void Dialog_FieldCalculation::on_button_test()
 
   for(Field::type_list_strings::const_iterator iter = triggered_relationships.begin(); iter != triggered_relationships.end(); ++iter)
   {
-    field_names += ( "related(" + *iter + ")[?], " );
+    field_names += ( "related(" + *iter + "), " );
   }
 
   m_label_triggered_by->set_text(field_names);
