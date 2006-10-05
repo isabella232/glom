97e818777b9382b3c6b6dfe2dc21e41772df98d4
Murray Cumming <murrayc@murrayc.com>
2006-10-05 09:54:48 +0000

    Added test.
    2006-10-05  Murray Cumming  <murrayc@murrayc.com>
    
            * glom/libglom/Makefile.am:
            * glom/libglom/test_sharedptr_layoutitem.cc: Added test.

---
+++ glom/libglom/test_sharedptr_layoutitem.cc
@@ -0,0 +1,53 @@
+/* Glom
+ *
+ * Copyright (C) 2001-2004 Murray Cumming
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+ 
+#include "glom/libglom/data_structure/layout/layoutitem_field.h"
+
+class FieldCreator
+{
+public:
+  Glom::sharedptr<Glom::LayoutItem_Field> get_field()
+  {
+    m_field = Glom::sharedptr<Glom::LayoutItem_Field>::create();
+    m_field->set_name("testname");
+    return m_field;
+  }
+protected:
+  Glom::sharedptr<Glom::LayoutItem_Field> m_field;
+};
+
+int
+main(int argc, char* argv[])
+{
+  Glom::sharedptr<Glom::LayoutItem_Field> field;
+  {
+     FieldCreator creator;
+     field = creator.get_field();
+  }
+
+  std::cout << "name=" << field->get_name() << std::endl;
+
+  return 0;
+}
+
+
+
+
+
