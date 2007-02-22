e45e366be406ff5e94ab45b70a093a85955d363d
Murray Cumming <murrayc@murrayc.com>
2007-02-22 12:06:25 +0000

    Add 5434 to the list of ports to try, because this is used in the current
    2007-02-22  Murray Cumming  <murrayc@murrayc.com>
    
            * glom/libglom/connectionpool.cc: Add 5434 to the list of ports to try, because this 
            is used in the current Ubuntu Feisty beta for Postgres 8.2, though this might be changed 
            in later Ubuntu Feisty betas. Also added 5435 and 5436 for the future.
    
    svn path=/trunk/; revision=1038

--- glom/libglom/connectionpool.cc
+++ glom/libglom/connectionpool.cc
@@ -137,8 +137,13 @@ ConnectionPool::ConnectionPool()
   m_avahi_client(0),
   m_avahi_mainloop(0)
 {
+  m_list_ports.push_back("5432"); //Ubuntu Breezy seems to default to this for Postgres 7.4, and this is probably the default for most postgres installations, including Fedora.
+
   m_list_ports.push_back("5433"); //Ubuntu Dapper seems to default to this for Postgres 8.1, probably to avoid a clash with Postgres 7.4
-  m_list_ports.push_back("5432");
+
+  m_list_ports.push_back("5434"); //Earlier versions of Ubuntu Feistry defaulted to this for Postgres 8.2.
+  m_list_ports.push_back("5435"); //In case Ubuntu increases the port number again in future.
+  m_list_ports.push_back("5436"); //In case Ubuntu increases the port number again in future.
 }
 
 ConnectionPool::~ConnectionPool()
