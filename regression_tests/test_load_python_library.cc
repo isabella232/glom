81a3324dabdfdacd0f2d183156811f5d621325b5
Murray Cumming <murrayc@murrayc.com>
2009-09-05 01:17:27 +0200

    Added missing file.

---
+++ regression_tests/test_load_python_library.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+int main()
+{
+  // Attempt to dynamically load the python module, 
+  // attempting to resolve all symbols immediately:
+  const char* path = "glom/python_embed/python_module/.libs/glom_1_12.so";
+  void* lib = dlopen(path, RTLD_NOW);
+
+  if(!lib)
+  {
+    char *error = dlerror();
+    if(error)
+    {
+      fprintf (stderr, "%s\n", error);
+    }
+
+    return EXIT_FAILURE;
+  }
+  else
+    dlclose(lib);
+
+  return EXIT_SUCCESS;
+}
