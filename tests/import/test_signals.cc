fe5cbe397994942453068ac27e1bcb6daead4dc8
Michael Hasselmann <michaelh@openismus.com>
2009-09-18 17:19:06 +0200

    Import tests: made them quiet
    * Makefile_tests.am, test/import/utils.[cc|h],
    tests/import/test_[parsing|signals].cc: Only report on failure. Moved common
    parts into an utility namespace.

--- tests/import/test_signals.cc
+++ tests/import/test_signals.cc
@@ -1,4 +1,5 @@
 #include <glom/import_csv.h>
+#include <tests/import/utils.h>
 #include <glibmm/regex.h>
 #include <iostream>
 #include <stdexcept>
@@ -7,13 +8,6 @@
 namespace {
 
 typedef std::vector<std::string> Encodings;
-
-/// This takes a @a size argument so we can test parsing of null bytes.
-void set_parser_contents(Glom::CsvParser& parser, const char* input, guint size)
-{
-  // Do not read terminating null byte.
-  parser.m_raw = std::vector<char>(input, input + size -1);
-}
 
 guint& get_line_scanned_count_instance()
 {
@@ -58,25 +52,23 @@ int main()
   parser.signal_line_scanned().connect(sigc::hide(sigc::hide(&on_line_scanned)));
   parser.signal_encoding_error().connect(sigc::ptr_fun(&on_encoding_error));
 
-  bool test_ignore_quoted_newlines = false;
-  bool test_ignore_empty_lines = false;
-  bool test_wrong_encoding = false;
-  bool test_incomplete_chars = false;
-
-  std::stringstream results;
+  bool result = true;
+  std::stringstream report;
 
   // test_ignore_quoted_newlines
   {
     // 2 CSV lines, first one contains newlines inside quotes
     const char raw[] = "\"some\n quoted\r\n newlines\n\", \"token2\"\n\"token3\"\n";
-    set_parser_contents(parser, raw, sizeof(raw));
+    ImportTests::set_parser_contents(parser, raw, sizeof(raw));
 
     while(parser.on_idle_parse())
     {}
 
-    results << "test_ignore_quoted_newlines: "
-            << (test_ignore_quoted_newlines = (2 == get_line_scanned_count_instance()))
-            << std::endl;
+    bool passed = (2 == get_line_scanned_count_instance() &&
+                   0 == get_encoding_error_count_instance());
+
+    if(!ImportTests::check("test_ignore_quoted_newlines", passed, report))
+      result = false;
 
     reset_signal_counts();
     parser.clear();
@@ -86,15 +78,16 @@ int main()
   {
     // 5 CSV lines, but only 2 contain data
     const char raw[] = "token1\n\n\n\ntoken2, token3\n";
-    set_parser_contents(parser, raw, sizeof(raw));
+    ImportTests::set_parser_contents(parser, raw, sizeof(raw));
 
     while(parser.on_idle_parse())
     {}
 
-    results << "test_ignore_empty_lines: "
-            << (test_ignore_empty_lines = (2 == get_line_scanned_count_instance() &&
-                                           0 == get_encoding_error_count_instance()))
-            << std::endl;
+    bool passed = (2 == get_line_scanned_count_instance() &&
+                   0 == get_encoding_error_count_instance());
+
+    if(!ImportTests::check("test_ignore_empty_lines", passed, report))
+      result = false;
 
     reset_signal_counts();
     parser.clear();
@@ -107,7 +100,7 @@ int main()
 
     // An invalid Unicode sequence.
     const char raw[] = "\0xc0\0x00\n";
-    set_parser_contents(parser, raw, sizeof(raw));
+    ImportTests::set_parser_contents(parser, raw, sizeof(raw));
 
     for (Encodings::const_iterator iter = encodings.begin();
          iter != encodings.end();
@@ -128,10 +121,12 @@ int main()
       parser.set_encoding((*iter).c_str());
     }
 
-    results << "test_wrong_encoding: "
-            << (test_wrong_encoding = (2 == get_encoding_error_count_instance() &&
-                                       0 == get_line_scanned_count_instance()))
-            << std::endl;
+
+    bool passed = (2 == get_encoding_error_count_instance() &&
+                   0 == get_line_scanned_count_instance());
+
+    if(!ImportTests::check("test_wrong_encoding", passed, report))
+      result = false;
 
     reset_signal_counts();
     parser.clear();
@@ -141,26 +136,24 @@ int main()
   {
     // An incomplete Unicode sequence.
     const char raw[] = "\0xc0\n";
-    set_parser_contents(parser, raw, sizeof(raw));
+    ImportTests::set_parser_contents(parser, raw, sizeof(raw));
 
     while(parser.on_idle_parse())
     {}
 
-    parser.clear();
+    bool passed = (1 == get_encoding_error_count_instance() &&
+                   0 == get_line_scanned_count_instance());
 
-    results << "test_incomplete_chars: "
-            << (test_incomplete_chars = (1 == get_encoding_error_count_instance() &&
-                                         0 == get_line_scanned_count_instance()))
-            << std::endl;
+    if(!ImportTests::check("test_incomplete_chars", passed, report))
+      result = false;
 
     reset_signal_counts();
     parser.clear();
   }
 
-  std::cout << results.rdbuf() << std::endl;
-  return (test_ignore_quoted_newlines &&
-          test_ignore_empty_lines &&
-          test_wrong_encoding &&
-          test_incomplete_chars
-         ) ? EXIT_SUCCESS : EXIT_FAILURE;
+  if(!result)
+    std::cout << report.rdbuf() << std::endl;
+
+  return result ? EXIT_SUCCESS : EXIT_FAILURE;
 }
+
