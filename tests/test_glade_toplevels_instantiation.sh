04d4072989bebee6fb433390d50b4b04ad6bd919
Murray Cumming <murrayc@murrayc.com>
2010-03-30 13:23:02 +0200

    Glade instantation test: Make it exit on failure.
    * tests/test_glade_toplevels_instantiation.sh: Use || exit 1, as in
        David's commit to the other files.

--- tests/test_glade_toplevels_instantiation.sh
+++ tests/test_glade_toplevels_instantiation.sh
@@ -3,5 +3,5 @@
 for x in $(find ${srcdir}/glom/ -name "*.glade")
 do
   # echo glade_toplevels_instantiation $x
-  tests/glade_toplevels_instantiation $x
+  tests/glade_toplevels_instantiation $x || exit 1
 done
