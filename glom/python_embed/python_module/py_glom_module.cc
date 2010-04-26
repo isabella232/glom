79c817255eeae93d99320aeb30e190c9b22a89dc
Murray Cumming <murrayc@murrayc.com>
2010-04-26 16:25:28 +0200

    Python Glom module: Improved the docstrings, using reStructuredText
    * glom/python_embed/python_module/py_glom_module.cc: Use more
        reStructureText (or as much as sphinx allows us to use in a docstring).
        It's looking quite good.

--- glom/python_embed/python_module/py_glom_module.cc
+++ glom/python_embed/python_module/py_glom_module.cc
@@ -36,15 +36,16 @@ BOOST_PYTHON_MODULE(glom_1_14)
     true, // show Python signatures.
     false); // Don't mention the C++ method signatures in the generated docstrings.
 
-  // Note: Our python docstring documentation is in ReStructuredText format, 
+  // Note: Our python docstring documentation is in ReStructuredText format,
   // because we use sphinx to generate the API reference documentation.
-  // However, sphinx does not seem to allow us to use regular reStructuredText 
-  // - section headings 
+  // However, sphinx does not seem to allow us to use regular reStructuredText
+  // - section headings
   // - :: at the end of a paragraph, instead of on a separate line.
   // TODO: Add # targets so we can link directly to parts such as Testing for Empty Values
-   
-  boost::python::class_<PyGlomRecord>("Record", 
-    "This is the  current record of the current table.\n"
+  // TODO: We can't seem to use reStuctureText sections.
+
+  boost::python::class_<PyGlomRecord>("Record",
+    "This is the  current record of the current table. A :class:`Record` object is passed to field calculations and button scripts, providing access to the values of the fields in that record. The :class:`Record` object passed to field calculations is read-only.\n"
     "\n"
     "Field values\n"
     "  Use record['field_name'] to get the value of a specified field in the current record. For instance, this concatenates the values of the name_first and name_last fields in the current record.\n"
@@ -52,28 +53,13 @@ BOOST_PYTHON_MODULE(glom_1_14)
     "\n"
     "    record['name_first'] + ' ' + record['name_last']\n"
     "\n"
-    "  You may also use this syntax to  set the value of a field in the current record. This is possible in a button script, but not in a field calculation. For instance, this sets the value of the name_first field in the current record.\n"
+    "  You may also use this syntax to set the value of a field in the current record. This is possible in a button script, but not in a field calculation. For instance, this sets the value of the name_first field in the current record.\n"
     "  ::\n"
     "\n"
     "    record['name_first'] = 'Bob'\n"
     "\n"
     "Related Records\n"
-    "  Use record.related['relationship_name'] to get the related records. For instance, this provides the related records for the current record.\n"
-    "  ::\n"
-    "\n"
-    "    record.related['location']\n"
-    "\n"
-    "Single Related Records\n"
-    "  For relationships that specify a single record, you can get the value of a field in that record. For instance, this is the value of the name field in the table indicated by the location relationship (often called location::name).\n"
-    "  ::\n"
-    "\n"
-    "    location_name = record.related['location']['name']\n"
-    "\n"
-    "Multiple Related Records\n"
-    "  For relationships that specify multiple records, you can use the aggregate functions to get overall values. For instance, this provides the sum of all total_price values from all of the lines of the current invoice record. See the :class:`RelatedRecord` class for more aggregate functions.\n"
-    "  ::\n"
-    "\n"
-    "    invoice_total = record.related['invoice_lines'].sum('total_price')\n"
+    "  Use the :attr:`related` attribute to access related records via a relationship."
     "\n"
      "Testing for Empty Values\n"
     "  How you test for empty values depends on the type of field.\n"
@@ -94,27 +80,50 @@ BOOST_PYTHON_MODULE(glom_1_14)
     "      if(record['name_full'] == ''):\n"
     "          return 'No Name'\n"
     "      else:\n"
-    "          return record['name_full']\n"
-    "\n"
-    )
-    .add_property("table_name", &PyGlomRecord::get_table_name,
-       "The name of the current table.")
-    .add_property("connection", &PyGlomRecord::get_connection,
-       "The current database connection for use with the pygda API.")
-    .add_property("related", &PyGlomRecord::get_related,
-       "Related records. Use the ['relationship_name'] notation with this object.")
+    "          return record['name_full']\n")
 
+    // boost::python doesn't mention the property types in the generated docstring,
+    // so we must mention it in the properties' docstring text.
+    .add_property("table_name", &PyGlomRecord::get_table_name,
+       "The name of the current table as a string.")
+    .add_property("connection", &PyGlomRecord::get_connection,
+       "The current database connection for use with the pygda API. This is a :class:`Gda.Connection` object.")
+    .add_property("related", &PyGlomRecord::get_related,
+       ":class:`Related` records. Use the ['relationship_name'] notation with this object.")
+
+    // These can have docstring text too, but sphinx ignores it,
+    // so we mention the [] syntax in the class docstring instead.
+    // TODO: Ask about that / file a sphinx bug.
     .def("__getitem__", &PyGlomRecord::getitem)
     .def("__setitem__", &PyGlomRecord::setitem)
     .def("__len__", &PyGlomRecord::len)
   ;
 
-  boost::python::class_<PyGlomRelated>("Related")
+  boost::python::class_<PyGlomRelated>("Related",
+    "This object provides access to related records via its [] syntax, which takes a relationship name and returns a :class:`RelatedRecord`. For instance, this provides the related records for the current record, via the :attr:`Record.related` attribute\n"
+    "::\n"
+    "\n"
+    "  record.related['location']")
+
     .def("__getitem__", &PyGlomRelated::getitem)
     .def("__len__", &PyGlomRelated::len)
   ;
 
-  boost::python::class_<PyGlomRelatedRecord>("RelatedRecord")
+  boost::python::class_<PyGlomRelatedRecord>("RelatedRecord",
+    "One or more related records, returned by the [] syntax on a :class:`Related` object, such as the :attr:`Record.related` attribute.\n"
+    "\n"
+    "Single Related Records\n"
+    "  For relationships that specify a single record, you can get the value of a field in that record by using the [] synax, providing a field name. For instance, this is the value of the name field in the table indicated by the location relationship (often called location::name).\n"
+    "  ::\n"
+    "\n"
+    "    record.related['location']['name']\n"
+    "\n"
+    "Multiple Related Records\n"
+    "  For relationships that specify multiple records, you can use the aggregate functions to get overall values. For instance, this provides the sum of all total_price values from all of the lines of the current invoice record. See the :class:`RelatedRecord` class for more aggregate functions.\n"
+    "  ::\n"
+    "\n"
+    "    record.related['invoice_lines'].sum('total_price')\n")
+
     .def("sum", &PyGlomRelatedRecord::sum, boost::python::args("field_name"),
       "Add all values of the field in the related records.")
     .def("count", &PyGlomRelatedRecord::sum, boost::python::args("field_name"),
@@ -128,7 +137,8 @@ BOOST_PYTHON_MODULE(glom_1_14)
   ;
 
   boost::python::class_<PyGlomUI>("UI",
-    "A collection of methods to programatically change the Glom UI, performing tasks that might otherwise be done by the user via the mouse and keyboard.")
+    "A collection of methods to programatically change the Glom UI, performing some tasks that might otherwise be done by the user via the mouse and keyboard.  A :class:`UI` object is passed to button scripts, allowing them to control the user interface.")
+
     .def("show_table_details", &PyGlomUI::show_table_details,
       boost::python::args("table_name", "primary_key_value"),
       "Navigate to the specified table, showing its details view for the specified record.")
