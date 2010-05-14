ca7e2e61d5584668d0b2f50d40f99bfbe48187d0
Daniel Borgmann <danielb@openismus.com>
2010-05-14 17:42:26 +0100

    Fix a crash when using find mode.
    * glom/application.cc: Don't access m_action_mode_data.
    * glom/application.h: Remove m_action_mode_data.

--- glom/application.cc
+++ glom/application.cc
@@ -1542,12 +1542,20 @@ void Application::existing_or_new_new()
 
 void Application::set_mode_data()
 {
-  m_action_mode_data->activate();
+  if (!m_pFrame)
+    return;
+
+  if (m_pFrame->m_Mode == Frame_Glom::MODE_Find)
+    m_action_mode_find->activate();
 }
 
 void Application::set_mode_find()
 {
-  m_action_mode_find->activate();
+  if (!m_pFrame)
+    return;
+
+  if (m_pFrame->m_Mode == Frame_Glom::MODE_Data)
+    m_action_mode_find->activate();
 }
 
 #ifndef GLOM_ENABLE_MAEMO
