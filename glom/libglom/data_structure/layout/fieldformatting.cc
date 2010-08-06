856bdf4f2271089fcda2322a9a17620a525373e4
Murray Cumming <murrayc@murrayc.com>
2010-08-06 17:58:03 +0200

    FieldFormatting: Default to show-all for related choices.
    * glom/libglom/data_structure/layout/fieldformatting.cc:

--- glom/libglom/data_structure/layout/fieldformatting.cc
+++ glom/libglom/data_structure/layout/fieldformatting.cc
@@ -37,7 +37,7 @@ FieldFormatting::FieldFormatting()
   m_text_format_multiline(false),
   m_text_multiline_height_lines(MULTILINE_TEXT_DEFAULT_HEIGHT_LINES),
   m_horizontal_alignment(HORIZONTAL_ALIGNMENT_AUTO),
-  m_choices_related_show_all(false)
+  m_choices_related_show_all(true) //Because this a the simpler, more often useful, default.
 {
 }
 
