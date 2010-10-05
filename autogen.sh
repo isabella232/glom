074193bb3f583c61f94955203ec0052c0ce8587f
Javier Jardón <jjardon@gnome.org>
2010-10-05 03:14:37 +0200

    Use upstream gettext instead Glib one
    	* configure.ac: Do not use AM_GLIB_GNU_GETTEXT() because it is no longer
    	maintained/recommended.
    	This fixes bug #631367

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,7 @@ test -n "$srcdir" || srcdir=.
   gnome-doc-common --copy &&
   gnome-doc-prepare --automake --copy --force &&
   mm-common-prepare --copy --force &&
+  autopoint --force &&
   AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
 ) || exit
 test -n "$NOCONFIGURE" || "$srcdir/configure" --enable-maintainer-mode "$@"
