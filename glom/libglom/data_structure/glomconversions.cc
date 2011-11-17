a704565d96e07d1ac19a02ced748eb68f69f7893
Murray Cumming <murrayc@murrayc.com>
2011-11-17 11:04:21 +0100

    date test: Set LANGUAGE too, which seems to be necessary on Ubuntu.
    * tests/test_glom_date_in_locales.sh: Also add some locales that
    are available on Ubuntu.
    
    Conflicts:
    
    	tests/test_glom_date_in_locales.sh

--- glom/libglom/data_structure/glomconversions.cc
+++ glom/libglom/data_structure/glomconversions.cc
@@ -106,6 +106,7 @@ static inline const char* glom_get_locale_date_format()
     c_locale_date_format = _("%x");
   }
 
+  //std::cout << G_STRFUNC << ": c_locale_date_format=" << c_locale_date_format << std::endl;
   return c_locale_date_format;
 }
 
@@ -185,6 +186,8 @@ bool Conversions::sanity_check_date_text_representation_uses_4_digit_years(bool 
   {
     //Note to translators: If you see this error in the terminal at startup then you need to translate the %x elsewhere.
     std::cerr << _("ERROR: sanity_check_date_text_representation_uses_4_digit_year(): Sanity check failed: Glom does not seem to use 4 digits to display years in a date's text representation, in this locale. Defaulting to dd/mm/yyyy though this might be incorrect for your locale. This needs attention from a translator. Please file a bug - see http://www.glom.org") << std::endl;
+
+    //std::cout << "  Unexpected date text: " << date_text << std::endl;
 
     //Do not depend on translators to do what we ask.
     //Default to a common format, though this would be incorrect in some 
