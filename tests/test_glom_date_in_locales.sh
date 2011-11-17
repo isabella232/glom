75611ce2cd83d400287bca1be8ed271d09d77cd2
Murray Cumming <murrayc@murrayc.com>
2011-11-17 11:04:21 +0100

    date test: Set LANGUAGE too, which seems to be necessary on Ubuntu.
    * tests/test_glom_date_in_locales.sh: Also add some locales that
    are available on Ubuntu.

--- tests/test_glom_date_in_locales.sh
+++ tests/test_glom_date_in_locales.sh
@@ -12,14 +12,15 @@
 # TODO: Get a list from po/*.po ?
 #
 # On debian/Ubuntu do this: 
-#  sudo apt-get install language-pack-de language-pack-es language-pack-fi language-pack-fr language-pack-hu language-pack-it language-pack-pt language-pack-sl
+#  sudo apt-get install language-pack-de language-pack-es language-pack-fi language-pack-fr language-pack-hu language-pack-it language-pack-pt language-pack-sl language-pack-da language-pack-cz language-pack-nb language-pack-se
 #
-# These are apparently not available on Fedora:  "da_DA.UTF-8" "cs_CZ.UTF-8" "nb_NO.UTF-8" "sv_SE.UTF-8"
-locales=("en_US.UTF-8" "en_GB.UTF-8" "en_CA.UTF-8" "de_DE.UTF-8" "es_ES.UTF-8" "fi_FI.UTF-8" "fr_FR.UTF-8" "hu_HU.UTF-8" "it_IT.UTF-8" "pt_PT.UTF-8" "pt_BR.UTF-8" "sl_SI.UTF-8")
+# These are apparently not available on Fedora:  "da_DK.UTF-8" "cs_CZ.UTF-8" "nb_NO.UTF-8" "sv_SE.UTF-8"
+locales=("en_US.UTF-8" "en_GB.UTF-8" "en_CA.UTF-8" "de_DE.UTF-8" "es_ES.UTF-8" "fi_FI.UTF-8" "fr_FR.UTF-8" "hu_HU.UTF-8" "it_IT.UTF-8" "pt_PT.UTF-8" "pt_BR.UTF-8" "sl_SI.UTF-8" "da_DK.UTF-8" "cs_CZ.UTF-8" "nb_NO.UTF-8" "sv_SE.UTF-8")
 
 for x in "${locales[@]}"
 do
-  echo testing with LANG="$x"
+  echo testing with LANG and LANGUAGE="$x"
   export LANG="$x"
+  export LANGUAGE="$x"
   ${buildir}glom --debug-date-check || exit 1
 done
