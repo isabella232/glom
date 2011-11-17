7a7bd2cfab67dca6326b6db6ea3115e8d16f884e
Murray Cumming <murrayc@murrayc.com>
2011-11-17 09:27:52 +0100

    locale test: Add apt-get hint

--- tests/test_glom_date_in_locales.sh
+++ tests/test_glom_date_in_locales.sh
@@ -9,8 +9,12 @@
 #
 # These are chosen based on problems found previously,
 # and the ones with good translations shown here: http://l10n.gnome.org/module/glom/
-#TODO: Get a list from po/*.po ?
-#These are apparently not available on Fedora:  "da_DA.UTF-8" "cs_CZ.UTF-8" "nb_NO.UTF-8" "sv_SE.UTF-8"
+# TODO: Get a list from po/*.po ?
+#
+# On debian/Ubuntu do this: 
+#  sudo apt-get install language-pack-de language-pack-es language-pack-fi language-pack-fr language-pack-hu language-pack-it language-pack-pt language-pack-sl
+#
+# These are apparently not available on Fedora:  "da_DA.UTF-8" "cs_CZ.UTF-8" "nb_NO.UTF-8" "sv_SE.UTF-8"
 locales=("en_US.UTF-8" "en_GB.UTF-8" "en_CA.UTF-8" "de_DE.UTF-8" "es_ES.UTF-8" "fi_FI.UTF-8" "fr_FR.UTF-8" "hu_HU.UTF-8" "it_IT.UTF-8" "pt_PT.UTF-8" "pt_BR.UTF-8" "sl_SI.UTF-8")
 
 for x in "${locales[@]}"
