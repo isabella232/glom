c3cefe607428a84bdf8de1b04e8bef6f70b04564
Murray Cumming <murrayc@murrayc.com>
2012-01-30 10:02:54 +0100

    Add translatable strings from OnlineGlom
    * glom/onlineglom_strings.cc:
    * Makefile.am:
    * po/POTFILES.in: Mention the new file.
    These strings are actually used here:
    http://gitorious.org/online-glom/gwt-glom/trees/master/src/main/resources/org/glom/web/client/ui
    and I must manually copy the translations across until I find a better way.

---
+++ glom/onlineglom_strings.cc
@@ -0,0 +1,33 @@
+/* Glom
+ *
+ * Copyright (C) 2001-2010 Murray Cumming
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <glibmm/i18n.h>
+
+// These strings are not actually used by Glom.
+// I am putting them here so that GNOME's translators can translate them
+// without dealing with the awkward (not gettext) system here:
+// http://www.murrayc.com/blog/permalink/2012/01/27/online-glom-more-translation/
+// murrayc
+
+const char* online_glom_search = _("Search");
+const char* online_glom_backToList = _("Back to List");
+const char* online_glom_details = _("Details");
+const char* online_glom_open = _("Open");
+
