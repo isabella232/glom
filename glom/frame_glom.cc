ed87c86414e04bb6fcac665e9107b5b195696030
Murray Cumming <murrayc@murrayc.com>
2012-02-22 09:57:36 +0100

    Fix typo in comment.

--- glom/frame_glom.cc
+++ glom/frame_glom.cc
@@ -2377,7 +2377,7 @@ void Frame_Glom::on_dialog_layout_print_hide()
     document->set_print_layout(m_table_name, print_layout);
   }
 
-  //Update the reports menu:
+  //Update the print layouts menu:
   AppWindow* pApp = dynamic_cast<AppWindow*>(get_app_window());
   if(pApp)
     pApp->fill_menu_print_layouts(m_table_name);
