c35ee0229614e783f3b4a21549eb8a71c890e1d0
Murray Cumming <murrayc@murrayc.com>
2013-04-03 01:51:33 +0200

    Deal with a minor TODO.
    	* glom/frame_glom.h: Make the signal handlers private.

--- glom/frame_glom.h
+++ glom/frame_glom.h
@@ -215,7 +215,7 @@ protected:
 
   void alert_no_table();
 
-  //TODO: Make these private?
+private:
   //Signal handlers:
   void on_notebook_find_criteria(const Gnome::Gda::SqlExpr& where_clause);
   void on_button_quickfind();
