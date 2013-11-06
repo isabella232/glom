7217d0c0b3186e191238dfa2a12de31b98064c59
Murray Cumming <murrayc@murrayc.com>
2013-11-06 11:06:12 +0100

    main.cc: Do not call gtk_init() now that we can use newer gtkmm.

--- glom/main.cc
+++ glom/main.cc
@@ -495,11 +495,6 @@ main(int argc, char* argv[])
     Glib::RefPtr<Glom::Application> application = 
       Glom::Application::create();
 
-    //Call gtk_init() too, because earlier (< 3.9.17) versions
-    //of gtkmm do not do this in the default Gtk::Application constructor.
-    //TODO: Remove this when we can depend on a newer gtkmm:
-    gtk_init(0, 0);
-
 #ifndef GLOM_ENABLE_CLIENT_ONLY
     Gsv::init();
     Goocanvas::init();
