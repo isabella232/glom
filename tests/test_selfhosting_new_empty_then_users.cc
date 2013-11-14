d5c990e5c497f5322649be043195b57260f3b38f
Murray Cumming <murrayc@murrayc.com>
2013-11-14 09:45:15 +0100

    Fix a typo in the header blocks.
    Some file had this typo and was probably then copied into new files.

--- tests/test_selfhosting_new_empty_then_users.cc
+++ tests/test_selfhosting_new_empty_then_users.cc
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public
  * License along with this program; if not, write to the
-71 * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301 USA.
  */
 
