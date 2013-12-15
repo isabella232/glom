50e5202d1320defae7b68f566bedf92f85c2d4c8
Murray Cumming <murrayc@murrayc.com>
2013-12-15 12:16:52 +0100

    autogen.sh: Add some comments.

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,9 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 (
   cd "$srcdir" &&
-  gnome-doc-common --copy &&
-  gnome-doc-prepare --automake --copy --force &&
-  mm-common-prepare --copy --force &&
+  gnome-doc-common --copy && # From gnome-common
+  gnome-doc-prepare --automake --copy --force && #From gnome-doc-utis
+  mm-common-prepare --copy --force && # From mm-common
   autopoint --force &&
   AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
 ) || exit
