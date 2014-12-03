5b29f48f45ec60498b96f667a1ca035c5a158d50
Murray Cumming <murrayc@murrayc.com>
2014-12-03 09:33:56 +0100

    TranslatableItem::get_translatable_type_name(): Handle Field Titles.
    Found by Coverity Scan as Logically Dead Code because there was
    a duplicate else.

--- glom/libglom/data_structure/translatable_item.cc
+++ glom/libglom/data_structure/translatable_item.cc
@@ -242,7 +242,7 @@ Glib::ustring TranslatableItem::get_translatable_type_name(enumTranslatableItemT
     return _("Table");
   else if(item_type == TRANSLATABLE_TYPE_LAYOUT_ITEM)
     return _("Layout Group");
-  else if(item_type == TRANSLATABLE_TYPE_CUSTOM_TITLE)
+  else if(item_type == TRANSLATABLE_TYPE_FIELD_TITLE)
     return _("Field Title");
   else if(item_type == TRANSLATABLE_TYPE_BUTTON)
     return _("Button");
