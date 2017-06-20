7a2f0f0551de134d8d931fd477a138b297b24636
Murray Cumming <murrayc@murrayc.com>
2017-06-20 20:37:18 +0200

    Document::change_relationship_name(): Avoid a vector copy.

--- glom/libglom/document/document.cc
+++ glom/libglom/document/document.cc
@@ -1085,8 +1085,8 @@ void Document::change_relationship_name(const Glib::ustring& table_name, const G
   const auto doctableinfo = get_table_info(table_name);
   if(doctableinfo)
   {
-    type_vec_relationships relationships = doctableinfo->m_relationships;
-      
+    const auto& relationships = doctableinfo->m_relationships;
+
     //Change the relationship name:
     auto iterRelFind = find_if_same_name(relationships, name);
     if(iterRelFind != relationships.end())
