5df154ac883d07cf67c848a198280efbb6b23d56
Murray Cumming <murrayc@murrayc.com>
2019-10-28 14:47:22 +0100

    1.33.1

--- glom/python_embed/python_module/py_glom_module.cc
+++ glom/python_embed/python_module/py_glom_module.cc
@@ -29,7 +29,7 @@
 
 using namespace Glom;
 
-BOOST_PYTHON_MODULE(glom_1_32)
+BOOST_PYTHON_MODULE(glom_1_34)
 {
   boost::python::docstring_options doc_options(
     true, // show the docstrings from here
